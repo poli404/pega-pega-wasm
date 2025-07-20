4c0f738c1f9c8b1f36d642e4094037d9d9f435b4
Poli404 <ra134539@uem.br>
2025-07-19 22:58:33 -0300

    tiros, vidas e tela inicial finalizados

--- main.c
+++ main.c
@@ -1,14 +1,10 @@
 #include "wasm4.h"
 #include <math.h>
-//#include <stdio.h>
-//#include <stdlib.h>
-
 
 typedef struct {
   int x;
   int y; // position
   int life; // after being caught, takes one down
-  char type; // 'c' chaser or 'p' prey -> talvez tirar isso
   int dx;
   int dy; // última direção de movimento
 } Player;
@@ -27,8 +23,10 @@ Shot shot2 = {0, 0, 0, 0, 0};
 
 // para fazer vários tiros, criar array de shots e iterar sobre eles, mas isso parece ser mt dificil
 
-Player player1 = {142, 142, 3, 'p', 0, 0};
-Player player2 = {10, 30, 3, 'c', 0, 0};
+Player player1 = {142, 142, 3, 0, 0};
+Player player2 = {10, 30, 3, 0, 0};
+
+int initial = 1;
 
 const uint8_t smiley[] = {
     0b11000011,
@@ -41,27 +39,71 @@ const uint8_t smiley[] = {
     0b11000011,
 };
 
+// substitui implementacao do sprintf que era muito grande
+// pra conseguir mostrar os pontos na tela
+void int_to_str(char *buffer, int n) {
+    int i = 0;
+    if (n == 0) {
+        buffer[i++] = '0';
+        buffer[i] = '\0';
+    return;
+    }
+    while (n > 0) {
+        buffer[i++] = (n % 10) + '0';
+        n /= 10;
+    }
+    buffer[i] = '\0';
+    for (int j = 0; j < i / 2; ++j) {
+        char tmp = buffer[j];
+        buffer[j] = buffer[i - j - 1];
+        buffer[i - j - 1] = tmp;
+    }
+}
+
+
 void start() {
     trace("start");
 }
 
 void drawBackground() {
     //BORDA DA ARENA DE COMBATE
-    text("Pega-Pega: the game", 5, 5);
+    //text("Pega-Pega: the game", 5, 5);
     *DRAW_COLORS = 0x4;
     rect(0, 20, 10, 140);
     rect(0, 20, 154, 10);
     rect(0, 150, 154, 10);
     rect(150, 20, 10, 160);
-    //rect(76, 76, 76, 10);
-    //rect(0, 100, 100, 10);
 }
 
-void draw(int px, int py, int cx, int cy) {
+void drawPlayers() {
     *DRAW_COLORS = 0x2;
-    blit(smiley, px, py, 8, 8, BLIT_1BPP);
+    blit(smiley, player1.x, player1.y, 8, 8, BLIT_1BPP);
     *DRAW_COLORS = 0x3;
-    blit(smiley, cx, cy, 8, 8, BLIT_1BPP);
+    blit(smiley, player2.x, player2.y, 8, 8, BLIT_1BPP);
+}
+
+void drawLives() {
+    *DRAW_COLORS = 0x2;
+    text("P1:", 20, 5);
+    char buffer[100];
+    int_to_str(buffer, player1.life);
+    text(buffer, 50, 5);
+
+    *DRAW_COLORS = 0x3;
+    text("P2:", 100, 5);
+    char buffer2[100];
+    int_to_str(buffer2, player2.life);
+    text(buffer2, 130, 5);
+}
+
+// tela final
+void endGame(){
+    *DRAW_COLORS = 0x1;
+    rect(0, 0, 200, 200);
+    *DRAW_COLORS = 0x1;
+    //text("GAME OVER!", 40, 76);
+    if (player1.life == 0) {*DRAW_COLORS = 0x3; text("Player 2 won!", 30, 76);}
+    if (player2.life == 0) {*DRAW_COLORS = 0x2; text("Player 1 won!", 30, 76);}
 }
 
 void verify_borders(Player *play) {
@@ -97,8 +139,8 @@ void fireShot(Player *shooter, Shot *shot) {
         shot->x = shooter->x + 4;
         shot->y = shooter->y + 4;
         // define a direção do tiro
-        shot->dx = shooter->dx;
-        shot->dy = shooter->dy;
+        shot->dx = shooter->dx * 2;
+        shot->dy = shooter->dy * 2;
         shot->active = 1;
     }
 }
@@ -117,8 +159,9 @@ void updateShot(Player *player, Shot *shot) {
         return;
     }
     // verifica se deu colisão com outro jogador
-    if (shot->x >= player->x && shot->x <= player->x &&
-        shot->y >= player->y && shot->y <= player->y) {
+    if (shot->x >= player->x && shot->x <= (player->x + 8) &&
+        shot->y >= player->y && shot->y <= (player->y + 8)) {
+
         shot->active = 0;
         player->life--;
         return;
@@ -128,81 +171,51 @@ void updateShot(Player *player, Shot *shot) {
 	rect(shot->x, shot->y, 2, 2);
 }
 
-// tela final
-// arrumar para mostrar quem venceu
-void endGame(){
-    *DRAW_COLORS = 0x4;
-    rect(0, 0, 154, 154);
-    *DRAW_COLORS = 0x1;
-    text("GAME OVER!", 0, 76);
-}
-
-// substitui implementacao do sprintf que era muito grande
-// pra conseguir mostrar os pontos na tela
-void int_to_str(char *buffer, int n) {
-    int i = 0;
-    if (n == 0) {
-        buffer[i++] = '0';
-        buffer[i] = '\0';
-    return;
-    }
-    while (n > 0) {
-        buffer[i++] = (n % 10) + '0';
-        n /= 10;
-    }
-    buffer[i] = '\0';
-    for (int j = 0; j < i / 2; ++j) {
-        char tmp = buffer[j];
-        buffer[j] = buffer[i - j - 1];
-        buffer[i - j - 1] = tmp;
-    }
-}
-
 void update() {   // desenha e limpa a tela
-    PALETTE[3] = 0xeb6b6f;
-    PALETTE[2] = 0x4a2480;
-    PALETTE[1] = 0xc53a9d;
-    PALETTE[0] = 0xfff6d3;
-
+    PALETTE[3] = 0xe870d0;
+    PALETTE[2] = 0xc10090;
+    PALETTE[1] = 0xd32eaf;
+    PALETTE[0] = 0xffb4f6;
+    
     uint8_t gamepad = *GAMEPAD1;
     uint8_t gamepad2 = *GAMEPAD2;
     
-    drawBackground();
-    //updateShot(prey);
-
-    // Player *prey = &player1;
-    // Player *chaser = &player2;
-    Player *p1 = &player1;
-    Player *p2 = &player2;
-
-    if ((*p1).life > 0 && (*p2).life > 0) {
-        move(p2, &gamepad2);
-        verify_borders(p2);
-        move(p1, &gamepad);
-        verify_borders(p1);
-        // tiro do p1
-        if (gamepad & BUTTON_1) {
-            fireShot(p1, &shot1);
+    if (initial) {
+        *DRAW_COLORS = 0x4;
+        text("Pega-Pega: the game", 5, 60);
+        text("chase each other\naround and fire!!", 15, 80);
+        if ((gamepad && BUTTON_1) || (gamepad2 && BUTTON_1)) {
+            initial = 0;
         }
-        // tiro do p2
-        if (gamepad2 & BUTTON_1) {
-            fireShot(p2, &shot2);
-        }
-        updateShot(p2, &shot1); // p2 atira no p1
-        updateShot(p1, &shot2); // p1 atira no p2
-        
-        draw(player1.x, player1.y, player2.x, player2.y);
-        
-        // mostrar vidas na tela
-        int l1 = player1.life;
-        int l2 = player2.life;
-        char buffer[100];
-        int_to_str(buffer, l1);
-        //sprintf(buffer, "P1:%d, P2:%d", player1.life, player2.life);
-        
-        text(buffer, 50, 10); // arrumar layout pra mostrar vidas mais bonitinho
-    }
-    else {
-        endGame();
+    } else {
+      
+      drawBackground();
+
+      Player *p1 = &player1;
+      Player *p2 = &player2;
+
+      if ((*p1).life > 0 && (*p2).life > 0) {
+          move(p2, &gamepad2);
+          verify_borders(p2);
+          move(p1, &gamepad);
+          verify_borders(p1);
+          // tiro do p1
+          if (gamepad & BUTTON_1) {
+              fireShot(p1, &shot1);
+          }
+          // tiro do p2
+          if (gamepad2 & BUTTON_1) {
+              fireShot(p2, &shot2);
+          }
+          updateShot(p2, &shot1); // p2 atira no p1
+          updateShot(p1, &shot2); // p1 atira no p2
+          
+          drawPlayers();
+          // mostrar vidas na tela
+          drawLives();
+      }
+      else {
+          endGame();
+      }
     }
 }
