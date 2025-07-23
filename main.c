754a2f54d99b054ce7eb9502a2deac7aa35bfdfb
Ana Paula <164682309+anapaulacrippa@users.noreply.github.com>
2025-07-23 11:00:33 -0300

    cleaning comments

--- main.c
+++ main.c
@@ -1,27 +1,26 @@
 #include "wasm4.h"
 #include <math.h>
 
+// positions and last directions
 typedef struct {
   int x;
-  int y; // position
-  int life; // after being caught, takes one down
+  int y;
+  int life;  // after being caught, takes one down
   int dx;
-  int dy; // última direção de movimento
+  int dy;
 } Player;
 
+// positions and directions
 typedef struct {
-// representa posições e direções dos tiros
     int x;
     int y;
-    int dx; // direcao
+    int dx;
     int dy;
     int active;
 } Shot;
 
 Shot shot1 = {0, 0, 0, 0, 0};
 Shot shot2 = {0, 0, 0, 0, 0};
-
-// para fazer vários tiros, criar array de shots e iterar sobre eles, mas isso parece ser mt dificil
 
 Player player1 = {142, 142, 3, 0, 0};
 Player player2 = {10, 30, 3, 0, 0};
@@ -39,8 +38,7 @@ const uint8_t smiley[] = {
     0b11000011,
 };
 
-// substitui implementacao do sprintf que era muito grande
-// pra conseguir mostrar os pontos na tela
+// replaces sprintf implementation to show player lives
 void int_to_str(char *buffer, int n) {
     int i = 0;
     if (n == 0) {
@@ -64,15 +62,14 @@ void start() {
     trace("start");
 }
 
+// edge of the combat arena
 void drawBackground() {
-    //BORDA DA ARENA DE COMBATE
-    //text("Pega-Pega: the game", 5, 5);
     *DRAW_COLORS = 0x4;
-    rect(0, 20, 10, 60); // left
+    rect(0, 20, 10, 60);  // left
     rect(0, 95, 10, 70);
-    rect(0, 20, 154, 10); // top
-    rect(0, 150, 154, 10); // bottom
-    rect(150, 20, 10, 60); // right
+    rect(0, 20, 154, 10);  // top
+    rect(0, 150, 154, 10);  // bottom
+    rect(150, 20, 10, 60);  // right
     rect(150, 95, 10, 70);
 }
 
@@ -97,7 +94,7 @@ void drawLives() {
     text(buffer2, 130, 5);
 }
 
-// tela final
+// end screen
 void endGame(){
     *DRAW_COLORS = 0x1;
     rect(0, 0, 200, 200);
@@ -108,7 +105,7 @@ void endGame(){
 }
 
 void verify_borders(Player *play) {
-    if ((*play).y > 77 && (*play).y < 88) { // verifies the tunnel
+    if ((*play).y > 77 && (*play).y < 88) {  // verifies the tunnel
         if ((*play).x > 154) {(*play).x = 0;}
         else if ((*play).x < 0) {(*play).x = 154;}
     }
@@ -120,8 +117,8 @@ void verify_borders(Player *play) {
     }
 }
 
+// updates position and records directions
 void move(Player *play, uint8_t *gamer) {
-// atualiza posição e registra direções
   if (*gamer & BUTTON_LEFT) {
         (*play).x--;
         (*play).dx = -1;
@@ -145,44 +142,44 @@ void fireShot(Player *shooter, Shot *shot) {
     if (shot->active == 0 && (shooter->dx != 0 || shooter->dy != 0)) {
         shot->x = shooter->x + 4;
         shot->y = shooter->y + 4;
-        // define a direção do tiro
+        // define shot direction
         shot->dx = shooter->dx * 2;
         shot->dy = shooter->dy * 2;
         shot->active = 1;
-        // faz barulinho de tiro :)
+        // shot sound
         tone(400 | (523 << 8), 20, 100, TONE_PULSE1 | TONE_MODE3);
     }
 }
 
 void updateShot(Player *player, Shot *shot) {
-    // verificar se tiro está ativo
+    // check if shooting is active
     if (!shot->active) {
         return;
     }
-    // move o tiro, atualizando a posição
+    // move shoot, update position
     shot->x += shot->dx;
     shot->y += shot->dy;
-    // verifica se saiu do campo
+    // check if shoot went out of bounds
     if (shot->x < 2 || shot->x > 154 || shot->y < 30 || shot->y > 142) {
         shot->active = 0;
         return;
     }
-    // verifica se deu colisão com outro jogador
+    // checks collision with another player
     if (shot->x >= player->x && shot->x <= (player->x + 8) &&
         shot->y >= player->y && shot->y <= (player->y + 8)) {
         shot->active = 0;
         player->life--;
-
-        // barulinho de acertou o tiro
+        // sound shot hitting
         tone(60, 10, 100, TONE_PULSE1 | TONE_PAN_LEFT | TONE_PAN_RIGHT);
         return;
     }
-    // desenhar o tiro
+    // draw shot
     *DRAW_COLORS = 0x4;
 	rect(shot->x, shot->y, 2, 2);
 }
 
-void update() {   // desenha e limpa a tela
+// draw screen
+void update() {
     PALETTE[3] = 0xe870d0;
     PALETTE[2] = 0xc10090;
     PALETTE[1] = 0xd32eaf;
@@ -196,11 +193,10 @@ void update() {   // desenha e limpa a tela
         text("Pega-Pega: the game", 5, 60);
         text("chase each other\naround and fire!", 15, 80);
         text("press \x80 to fire", 20, 100);
-        if ((gamepad & BUTTON_1) || (gamepad2 & BUTTON_1)) { // X BUTTON
+        if ((gamepad & BUTTON_1) || (gamepad2 & BUTTON_1)) {  // X button
             initial = 0;
         }
     } else {
-      
       drawBackground();
 
       Player *p1 = &player1;
@@ -211,24 +207,23 @@ void update() {   // desenha e limpa a tela
           verify_borders(p2);
           move(p1, &gamepad);
           verify_borders(p1);
-          // tiro do p1
+          // p1 shot
           if (gamepad & BUTTON_1) {
               fireShot(p1, &shot1);
           }
-          // tiro do p2
+          // p2 shot
           if (gamepad2 & BUTTON_1) {
               fireShot(p2, &shot2);
           }
           updateShot(p2, &shot1); // p2 shots p1
           updateShot(p1, &shot2); // p1 shots p2
-          
-          drawPlayers();
           // shows the players and lives on screen
+          drawPlayers();
           drawLives();
       }
       else {
           endGame();
-          if ((gamepad & BUTTON_2) || (gamepad2 & BUTTON_2)) { //Z BUTTON
+          if ((gamepad & BUTTON_2) || (gamepad2 & BUTTON_2)) {  // Z button
             initial = 1;
             player1.life = 3; player2.life = 3;
             player1.dx = 0; player1.dy = 0;
